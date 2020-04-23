8c227112badc79b6a4207bca7b83ceac16aa649c
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-04-23 11:34:33 +0100

    Enum e Classes

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -8,13 +8,13 @@ namespace MyGame
 
         public Enemy(string name)
         {
-            SetName(name);
+            this.name = SetName(name);
             health = 100;
             shield = 0;
         }
 
         public string GetName() => name;
-
+            
         public string SetName(string name) 
         {
             this.name = name.Trim();
@@ -35,6 +35,19 @@ namespace MyGame
                 shield = 0;
                 health -= demageLeft;
                 if(health < 0) health = 0;
+            }
+        }
+
+        public void PickupPowerUp(PowerUp powerUp, float gain)
+        {
+            if(powerUp == PowerUp.Health)
+            {
+                health+= gain;
+            }
+            else if (powerUp == PowerUp.Shield)
+            {
+                shield += gain;
+                if(shield > 100) shield = 100;
             }
         }
         
--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,9 +6,39 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Enemy enemy1 = new Enemy ("   Ractan");
-            //Enemy enemy2 = new Enemy ("Rácten",100,100);
-            Console.WriteLine(enemy1.GetName());
+            //Declaração de variáveis
+            int numEnemies;
+            Enemy [] enemies;
+            Random random = new Random();
+            // Pedir ao utilizador o num de enimigos
+            Console.Write("Insira nº de inimigos: ");
+            numEnemies = int.Parse(Console.ReadLine());
+
+            //para cada inimigo pedir ao utilizador o seu nome,
+            //criar objeto inimigo e guardá-lo num array
+            enemies = new Enemy[numEnemies];
+            for(int i = 0; i < numEnemies; i++)
+            {
+                string name;
+                Console.Write($"Insira nome do inimigo #{i}:");
+                name = Console.ReadLine();
+                enemies[i] = new Enemy(name);
+
+                if(random.NextDouble() < 0.3)
+                {
+                    enemies[i].PickupPowerUp(PowerUp.Shield, 50);
+                }
+                else
+                {
+                    enemies[i].TakeDamage(random.Next(100));
+                }
+            }
+
+            foreach(Enemy enemy in enemies)
+            {
+                Console.WriteLine($">>{enemy.GetName()}<< HP={enemy.GetHealth()} Shield={enemy.GetShield()}<<");
+            }
+
         }
     }
 }
